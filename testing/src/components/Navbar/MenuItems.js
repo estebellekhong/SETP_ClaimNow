3cdcff3cb282dfc2d7441874950a8873055ef550
EzlikeRongYi <ezlimrongyi@gmail.com>
2022-08-07 21:45:00 +0800

    File Clean Up
    Shift all archived folders to 'Testing'
    
    Co-Authored-By: shinkag <108576402+shinkag@users.noreply.github.com>
    Co-Authored-By: Kai <98445120+WWKDev@users.noreply.github.com>

---
+++ testing/src/components/Navbar/MenuItems.js
@@ -0,0 +1,14 @@
+export const MenuItems = [
+  
+  
+    {
+        title: 'Contact Us',
+        url: 'index.html',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
